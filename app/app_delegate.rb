e0edf98b267787f2a2e20d64c331f320a9a89ba5
Ivan Storck <ivanoats@gmail.com>
2013-11-24 15:14:07 -0800

    simple menu bar mac os x app to show hide invisible files

---
+++ app/app_delegate.rb
@@ -0,0 +1,41 @@
+class AppDelegate
+  attr_accessor :status_menu
+
+  def applicationDidFinishLaunching(notification)
+    @app_name = NSBundle.mainBundle.infoDictionary['CFBundleDisplayName']
+
+    @status_menu = NSMenu.new
+
+    @status_item = NSStatusBar.systemStatusBar.statusItemWithLength(NSVariableStatusItemLength).init
+    @status_item.setMenu(@status_menu)
+    @status_item.setHighlightMode(true)
+    @status_item.setTitle(@app_name)
+
+    @status_menu.addItem createMenuItem("About #{@app_name}", 'orderFrontStandardAboutPanel:')
+    @status_menu.addItem createMenuItem("Hide Invisibles", 'hideInvisibles')
+    @status_menu.addItem createMenuItem("Show Invisibles", 'showInvisibles')
+    @status_menu.addItem createMenuItem("Quit", 'terminate:')
+  end
+
+  def createMenuItem(name, action)
+    NSMenuItem.alloc.initWithTitle(name, action: action, keyEquivalent: '')
+  end
+
+  def hideInvisibles
+    alert = NSAlert.alloc.init
+    `defaults write com.apple.finder AppleShowAllFiles FALSE`
+    `killall Finder`
+    alert.setMessageText "Invisible files are now hidden"
+    alert.addButtonWithTitle "OK"
+    alert.runModal
+  end
+  
+  def showInvisibles
+    `defaults write com.apple.finder AppleShowAllFiles TRUE`
+    `killall Finder`
+    alert = NSAlert.alloc.init
+    alert.setMessageText "Invisible files are now shown"
+    alert.addButtonWithTitle "Radical"
+    alert.runModal
+  end
+end
